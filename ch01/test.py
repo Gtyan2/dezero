f458aebc20ee731f55ff87c4c5e40b5216e3f5e1
Gtyan2 <chart205@outlook.jp>
2023-05-02 18:02:37 +0900

    本と同じコードに変更

--- ch01/test.py
+++ ch01/test.py
@@ -9,3 +9,15 @@ for n in range(1, 11):
     rewards.append(reward)
     Q = sum(rewards) / n
     print(Q)
+
+print('---')
+
+# incremental implementation
+np.random.seed(0)
+Q = 0
+
+for n in range(1, 11):
+    reward = np.random.rand()
+    #Q += (reward - Q) / n
+    Q = Q + (reward - Q) / n
+    print(Q)
